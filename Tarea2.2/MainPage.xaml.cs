40f79f2c828da6309276b00ed9bc26cc3c26b9ec
Daniela Reyes <reyesdaniela498@gmail.com>
2024-03-04 23:01:08 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2.2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+using System.Collections.ObjectModel;
+
+namespace Tarea2._2
+
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+
+            var drawingView = new DrawingView
+            {
+                Lines = new ObservableCollection<IDrawingLine>(),
+                LineColor = Colors.Red,
+                LineWidth = 5
+            };
+
+
+        }
+    }
+}
